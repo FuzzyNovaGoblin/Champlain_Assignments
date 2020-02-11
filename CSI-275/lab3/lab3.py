4326701c779a055dd53a0f41c5b7b7d17864ad24
FuzzyNovaGoblin <fuzzy.nova.goblin@outlook.com>
2020-02-11 17:08:08 -0500

    CSI-275 lab5

--- CSI-275/lab3/lab3.py
+++ CSI-275/lab3/lab3.py
@@ -17,72 +17,76 @@ assignment may, for the purpose of assessing this assignment:
 
 import socket
 
+
 def build_list():
-   """Asks user for numbers and returns a list
+    """Asks user for numbers and returns a list
 
-   The user is prompted to enter a number repetedly until an empty
-   string is returned. Every line that can be cast to an int or float
-   is added to a list.
-   """
-   done = False
-   nums = []
+    The user is prompted to enter a number repetedly until an empty
+    string is returned. Every line that can be cast to an int or float
+    is added to a list.
+    """
+    done = False
+    nums = []
 
-   while not done:
-      buff = input("Enter a number or just press Enter to stop\n")
-      if buff == "":
-         done = True
-      else:
-         try:
-            nums.append(int(buff))  # try to cast to int
-         except:
+    while not done:
+        buff = input("Enter a number or just press Enter to stop\n")
+        if buff == "":
+            done = True
+        else:
             try:
-               nums.append(float(buff))  # try to cast to int
+                nums.append(int(buff))  # try to cast to int
             except:
-               print("That is not a number")
+                try:
+                    nums.append(float(buff))  # try to cast to int
+                except:
+                    print("That is not a number")
 
-   return nums
+    return nums
+
 
 def sort_list(arr):
-   """sorts a list
+    """sorts a list
 
-   calls the built in list sort method on the argument
-   """
-   if len(arr) == 0:  # if the array is empty end function
-      return arr
+    calls the built in list sort method on the argument
+    """
+    if len(arr) == 0:  # if the array is empty end function
+        return arr
 
-   # turn the array into a string
-   arr_str = "LIST"
-   for s in arr:
-      arr_str += " " +str(s)
+    # turn the array into a string
+    arr_str = "LIST"
+    for s in arr:
+        arr_str += " " + str(s)
 
-   # encode the string to be sent to the server
-   byte_str = arr_str.encode("ascii")
+    # encode the string to be sent to the server
+    byte_str = arr_str.encode("ascii")
 
-   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # create socket
-   sock.connect(("192.168.3.122", 7778))  # connect to server
-   sock.sendall(byte_str)  # send our array to server
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # create socket
+    sock.connect(("192.168.3.122", 7778))  # connect to server
+    sock.sendall(byte_str)  # send our array to server
 
-   return_str = sock.recv(4096)  # get data from server
-   return_str = return_str.decode("ascii")  # decode the data from the server
-   sock.close()  # close the socket
+    return_str = sock.recv(4096)  # get data from server
+    return_str = return_str.decode("ascii")  # decode the data from the server
+    sock.close()  # close the socket
 
-   str_arr = return_str.split(" ")  # convert data to array
-   arr = str_arr[1:]  # remove key word from data
+    str_arr = return_str.split(" ")  # convert data to array
+    arr = str_arr[1:]  # remove key word from data
 
-   for i in range(len(arr)):
-      try:
-         arr[i] = int(arr[i])
-      except:
-         try:
-            arr[i] = float(arr[i])
-         except:
-            print("That is not a number")
+    for i in range(len(arr)):
+        try:
+            arr[i] = int(arr[i])
+        except:
+            try:
+                arr[i] = float(arr[i])
+            except:
+                print("That is not a number")
 
-   return arr
+    return arr
+
 
 def main():
-   arr = sort_list(build_list())
-   print(arr)
+    arr = sort_list(build_list())
+    print(arr)
+
 
 if __name__ == "__main__":
-   main()
+    main()
---
+++ CSI-275/lab5/server.py
@@ -0,0 +1,141 @@
+"""Grnat Hedley's code for Lab4.
+
+Author: Grant Hedley
+Class: CSI-275-1
+Assignment: Lab 4
+Due Date: 2/10/20
+
+Certification of Authenticity:
+I certify that this is entirely my own work, except where I have given
+fully-documented references to the work of others. I understand the definition
+and consequences of plagiarism and acknowledge that the assessor of this
+assignment may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of academic
+- staff; and/or Communicate a copy of this assignment to a plagiarism checking
+- service (which may then retain a copy of this assignment on its database for
+- the purpose of future plagiarism checking)
+"""
+
+
+import socket
+
+
+HOST = "localhost"
+PORT = 20000
+
+
+class ERROR(Exception):
+    pass
+
+
+def arr_to_str(arr):
+    str = ""
+    for i in range(len(arr)):
+        if i != 0:
+            str += " "
+        str += arr[i]
+    return str
+
+
+def to_lower_case_compare(str1, str2, opp="a"):
+    try:
+        str1 = int(str1)
+    except (ValueError):
+        try:
+            str1 = float(str1)
+        except (ValueError):
+            str1 = str1.lower()
+    try:
+        str2 = int(str2)
+    except (ValueError):
+        try:
+            str2 = float(str2)
+        except (ValueError):
+            str2 = str2.lower()
+    if opp == "s":
+        str1 = str(str1)
+        str2 = str(str2)
+    elif (type(str1) is str or
+            type(str2) is str)and not type(str1) is type(str2):
+        raise ERROR
+
+    if opp == "d":
+        return str1 < str2
+    return str1 > str2
+
+
+def sort_list(arr, operator):
+    """Sorts a list.
+
+    calls the built in list sort method on the argument
+    """
+    swaped = True  # = len(arr) - 1
+    while swaped:  # > 0:
+        swaped = False
+        for i in range(len(arr)-1):
+            if to_lower_case_compare(arr[i], arr[i + 1], operator):
+                swaped = True
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+    return arr
+
+
+def handle_data(data):
+    data = data.split(" ")
+    operator = "a"
+    if data[0] != "LIST":
+        return "ERROR"
+    data = data[1:]
+    if len(data) == 0:
+        return "ERROR"
+    elif "|" in data[-1]:
+        operator = data[-1][-1]
+        data[-1] = data[-1][:-2]
+        if not set({operator}).issubset(set({'a', 'd', 's'})):
+            return "ERROR"
+    print("operator:", operator)
+    print(data)
+
+    while data.__contains__(''):  # removes empty parts of the line
+        data.remove('')
+    try:
+        sort_list(data, operator)
+    except (ERROR):
+        return "ERROR"
+    data.insert(0, "SORTED")
+    data = arr_to_str(data)
+    return data
+
+
+class SortServer:
+    """Server to sort arrays"""
+
+    def __init__(self, host, port):
+        """Initialize the SortServer class
+
+        host and port are saved and a tcp socket is created
+        """
+
+        self.address = (host, port)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind(self.address)
+        self.sock.listen(20)
+
+    def run_server(self):
+        """Don't forget your docstring!"""
+        while True:
+            print("listening at", self.address)
+            cli_sock, address = self.sock.accept()
+            while True:
+                cli_sock.settimeout(5)
+                data = cli_sock.recv(4096)
+                data = data.decode("ASCII")
+                print('recived:', data)
+                data = handle_data(data)
+                print('return:', data)
+                cli_sock.sendall(data.encode("ascii"))
+                # cli_sock.close()
+
+
+if __name__ == "__main__":
+    server = SortServer(HOST, PORT)
+    server.run_server()
