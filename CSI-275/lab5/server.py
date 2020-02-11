ae6ec6cb8c1fb2cda1ac908d20d760bcd0e48423
FuzzyNovaGoblin <fuzzy.nova.goblin@outlook.com>
2020-02-11 17:26:47 -0500

    CSI-275 lab5

--- CSI-275/lab5/server.py
+++ CSI-275/lab5/server.py
@@ -25,10 +25,12 @@ PORT = 20000
 
 
 class ERROR(Exception):
+    """Throw if "ERROR" needs to be returned to client."""
     pass
 
 
 def arr_to_str(arr):
+    """Turns an array into a str"""
     str = ""
     for i in range(len(arr)):
         if i != 0:
@@ -38,6 +40,13 @@ def arr_to_str(arr):
 
 
 def to_lower_case_compare(str1, str2, opp="a"):
+    """Compares elements of an array.
+
+    if 's' operator is used converts paramaters to strings and
+    compares alphabeticaly, if 'a' or 'd' values are sorted assendingly or
+    decendingly, trys cast values to ints or floats before comparing
+    if some values are numbers and some are strings raises ERROR
+    """
     try:
         str1 = int(str1)
     except (ValueError):
@@ -67,7 +76,8 @@ def to_lower_case_compare(str1, str2, opp="a"):
 def sort_list(arr, operator):
     """Sorts a list.
 
-    calls the built in list sort method on the argument
+    bubble sort algorithem that uses to_lower_case_compare to
+    compare the values
     """
     swaped = True  # = len(arr) - 1
     while swaped:  # > 0:
@@ -80,6 +90,14 @@ def sort_list(arr, operator):
 
 
 def handle_data(data):
+    """Handle data sent to server.
+
+    checks that data starts with LIST key word and handles weather or not it
+    uses the paramater at the end of the data
+    if the data is valid it is sorted and "SORTED" is put infrount of it before
+    it is returned
+    otherwise "ERROR" is returned
+    """
     data = data.split(" ")
     operator = "a"
     if data[0] != "LIST":
@@ -92,8 +110,6 @@ def handle_data(data):
         data[-1] = data[-1][:-2]
         if not set({operator}).issubset(set({'a', 'd', 's'})):
             return "ERROR"
-    print("operator:", operator)
-    print(data)
 
     while data.__contains__(''):  # removes empty parts of the line
         data.remove('')
@@ -107,7 +123,7 @@ def handle_data(data):
 
 
 class SortServer:
-    """Server to sort arrays"""
+    """Server to sort arrays."""
 
     def __init__(self, host, port):
         """Initialize the SortServer class
@@ -121,19 +137,25 @@ class SortServer:
         self.sock.listen(20)
 
     def run_server(self):
-        """Don't forget your docstring!"""
+        """Start server.
+
+        starts a server on its given port and ip address and handles data as
+        it is sent to the server
+        """
         while True:
             print("listening at", self.address)
             cli_sock, address = self.sock.accept()
+            print("Connected to", address)
             while True:
-                cli_sock.settimeout(5)
                 data = cli_sock.recv(4096)
                 data = data.decode("ASCII")
+                if len(data) == 0:
+                    cli_sock.close()
+                    break
                 print('recived:', data)
                 data = handle_data(data)
                 print('return:', data)
                 cli_sock.sendall(data.encode("ascii"))
-                # cli_sock.close()
 
 
 if __name__ == "__main__":
