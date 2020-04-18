9e41c820a979f9cfe0f6f3ff60e0548ecc382061
FuzzyNovaGoblin <fuzzy.nova.goblin@outlook.com>
2020-04-18 16:56:38 -0400

    CSI-281 fianl stuff (^_^)!!

--- CSI-281/final/hashmap.h
+++ CSI-281/final/hashmap.h
@@ -1,66 +1,61 @@
 #ifndef HASHMAP_H
 #define HASHMAP_H
-#define SHIFT_V 20
 
 #include <string>
 #include "DynamicArray.h"
 #include <math.h>
-#include <functional>
 
 using namespace std;
+
+#define SHIFT_V 18
 
 const size_t PHI_MULTIPLYER = 11400714819323198485llu;
 const short int BIT_SHIFT = 64 - SHIFT_V;
 const int ARRAY_SIZE = pow(2, SHIFT_V);
-// const int ARRAY_SIZE = 1048576;
 
 class HashMap
 {
 private:
-   DynamicArray<ElementNode<string, int>*> **mElements;
-   DynamicArray<ElementNode<string, int>*> **arrHold;
+   DynamicArray<ElementNode<string, int> *> **mElements;
+   DynamicArray<ElementNode<string, int> *> **arrHold;
    int iterator;
-  // int loc;
-   // int iSize;
-   //TODO: remove this
-   //int tst = 0;
-
 
 public:
    HashMap();
    ~HashMap();
-   // static int hash(string str);
-   std::hash<string> hash;
-   static size_t hashToFib(size_t _hash);
-   int& operator[](const string& str);
-   int& get(const string& str);
+   static size_t hashFunc(const string str);
+   static size_t hashToFib(size_t hash);
+   int &operator[](const string &str);
+   int &get(const string &str);
 };
-HashMap::HashMap(){
-   mElements = new DynamicArray<ElementNode<string,int>*>*[ARRAY_SIZE];
+
+HashMap::HashMap()
+{
+   mElements = new DynamicArray<ElementNode<string, int> *> *[ARRAY_SIZE];
 }
 
-// int HashMap::hash(string str)
-// {
-//    int sum = 0;
-//    for (int i = 0; i < str.length(); i++)
-//    {
-//       sum += (int)(str[i]) * (i + 1) *(i * str.length());
-//    }
-//    return sum;
-// }
 
-size_t HashMap::hashToFib(size_t hash){
+size_t HashMap::hashFunc(const string str)
+{
+   size_t sum;
+
+   for (int i = 0; i < str.length(); i++)
+   {
+      sum += (str[i] * PHI_MULTIPLYER) ^ (sum >> 23);
+   }
+
+   return sum ^ (sum << 37);
+}
+
+size_t HashMap::hashToFib(size_t hash)
+{
    return (hash * PHI_MULTIPLYER) >> BIT_SHIFT;
 }
 
-// int& MyHashmap::operator[](const string& key){
-//    return mElements[hashToFib(hash(key))].get(key);
-// }
 
-int& HashMap::get(const string& key){
-   // loc = hashToFib(hash(key));
-   //cout <<hashToFib(hash(key))<< endl;
-   arrHold = &(mElements[hashToFib(hash(key))]);
+int &HashMap::get(const string &key)
+{
+   arrHold = &(mElements[hashToFib(hashFunc(key))]);
    if (*arrHold == NULL)
    {
       *arrHold = new DynamicArray<ElementNode<string, int> *>;
