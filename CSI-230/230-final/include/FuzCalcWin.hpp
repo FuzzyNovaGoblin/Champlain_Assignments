12a8358338b89836cfc58cc9a2a1040dac4470bb
FuzzyNovaGoblin <fuzzy.nova.goblin@outlook.com>
2020-12-07 11:35:25 -0500

    FuzCalc init

---
+++ CSI-230/230-final/include/FuzCalcWin.hpp
@@ -0,0 +1,9 @@
+/**
+ * @file FuzCalcWin.hpp
+ * @author Grant Hedley (FuzzyNovaGoblin@fuzytech.com)
+ * @brief dinfine the main instenc of the window for the FuzyCalc package
+ * @version 0.1
+ * @date 2020-12-07
+ */
+
+class FuzCalcWin {};
---
+++ CSI-230/230-final/src/FuzCalcWin.cc
@@ -0,0 +1,7 @@
+/**
+ * @file FuzCalcWin.cc
+ * @author Grant Hedley @FuzzyNovaGoblin
+ * @brief handles the window of the fuzycalc package
+ * @version 0.1
+ * @date 2020-12-07
+ */
