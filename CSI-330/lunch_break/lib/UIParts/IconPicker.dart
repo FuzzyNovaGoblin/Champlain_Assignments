9b5360adb8067d94f9f0ee53487343f8073f7ab4
FuzzyNovaGoblin <fuzzy.nova.goblin@outlook.com>
2021-02-12 12:15:46 -0500

    made back button a FAB

--- CSI-330/lunch_break/lib/UIParts/IconPicker.dart
+++ CSI-330/lunch_break/lib/UIParts/IconPicker.dart
@@ -4,10 +4,15 @@ class IconPicker extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.white,
+        child: Icon(Icons.arrow_back, color: Colors.black,),
+        onPressed: () =>Navigator.pop(context),),
       body: GridView.count(
         crossAxisCount: 4,
         // itemCount: 11309,
-        children:<Widget>[ElevatedButton(style:ElevatedButton.styleFrom(primary: Colors.red) , onPressed: ()=>Navigator.pop(context), child: Text("Cancel"))]+ List<Widget>.generate(
+        // /<Widget>[ElevatedButton(style:ElevatedButton.styleFrom(primary: Colors.red) , onPressed: ()=>Navigator.pop(context), child: Text("Cancel"))]+
+        children: List<Widget>.generate(
             11309, (index) => FittedBox(fit: BoxFit.contain, child: IconButton(onPressed: () => Navigator.pop(context, IconData(0xe52a + index, fontFamily: 'MaterialIcons')), icon: Icon(IconData(0xe52a + index, fontFamily: 'MaterialIcons'))))),
         // itemBuilder: (context, index) => Icon(IconData(0xe52a + index, fontFamily: 'MaterialIcons')),
       ),
